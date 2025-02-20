62ac76185400d2acf64b65112fe27070ce5e5462
GregRL94 <largreg1994@gmail.com>
2025-02-20 14:57:38 -0500

    Finalized Player Movement
    The player character now rotates and moves toward a virtual point computed from joystick input. Speed is controlled by the inclination of the joystick

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -6,22 +6,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0f, 20f)] public float force;
-    [Range(0f, 20)] public float maxSpeed;
+    [Range(0f, 20f)] public float speed;
+    [Range(0f, 10f)] public float rotationSpeed;
 
     private Rigidbody rb;
+    private float joystickPointDisplayDistance = 2;
+
+    Vector3 joystickVirtualPoint;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        var controllers = Input.GetJoystickNames();
     }
 
     void FixedUpdate()
     {
         Vector2 joystickInput = Gamepad.current.leftStick.ReadValue();
+        float joystickInputMagnitude = Mathf.Sqrt(joystickInput.x * joystickInput.x + joystickInput.y * joystickInput.y);
 
-        rb.AddForce(joystickInput.x * force, 0f, joystickInput.y * force, ForceMode.Force);
-        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
+        joystickVirtualPoint = new Vector3(transform.position.x + joystickInput.x * joystickPointDisplayDistance, transform.position.y, transform.position.z + joystickInput.y * joystickPointDisplayDistance);
+        transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, joystickVirtualPoint - transform.position, rotationSpeed * Time.deltaTime, 0.0f));
+        rb.velocity = transform.forward * joystickInputMagnitude * speed;        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(joystickVirtualPoint, 0.5f);
     }
 }
