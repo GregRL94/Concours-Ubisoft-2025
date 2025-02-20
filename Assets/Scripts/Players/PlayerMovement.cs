3410e295fb817010cb68f4df7bcc2afdf55b3e07
GregRL94 <largreg1994@gmail.com>
2025-02-20 00:46:50 -0500

    Changed player movement control method
    Changed player control mode from velocity based to force based

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -1,28 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0f, 10f)] public float speed;
+    [Range(0f, 20f)] public float force;
+    [Range(0f, 20)] public float maxSpeed;
 
     private Rigidbody rb;
-    private float speedFactor = 500f;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        var controllers = Input.GetJoystickNames();      
+        var controllers = Input.GetJoystickNames();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 joystickInput = Gamepad.current.leftStick.ReadValue();
-        float finalSpeed = speed * speedFactor * Time.deltaTime;
 
-        rb.velocity = new Vector3(joystickInput.x * finalSpeed, rb.velocity.y, joystickInput.y * finalSpeed);
+        rb.AddForce(joystickInput.x * force, 0f, joystickInput.y * force, ForceMode.Force);
+        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
     }
 }
