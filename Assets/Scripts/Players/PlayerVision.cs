cda4594cf75bd8f0d90c9c0793a8be37e88a07b5
GregRL94 <largreg1994@gmail.com>
2025-03-21 00:57:47 -0400

    Finished mesh generation

--- Assets/Scripts/Players/PlayerVision.cs
+++ Assets/Scripts/Players/PlayerVision.cs
@@ -20,30 +20,21 @@ public class PlayerVision : MonoBehaviour
     [Space]
     [Header("DETECTION RAYS")]
     [SerializeField] int _numberOfRays;
-    [SerializeField] private float _offset;
 
     private Mesh _foVMesh;
 
     // Start is called before the first frame update
     void Start()
     {
-        _foVMesh = new Mesh{name = "FoV Mesh"};
+        _foVMesh = new Mesh{name = "Field Of View Mesh"};
         _foVMeshFilter.mesh = _foVMesh;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        float startAngle = -_visionConeAngle / 2;
-        float stepAngle = _visionConeAngle / _numberOfRays;
-        float currentAngle = startAngle;
-
-        for (int i = 0; i <= _numberOfRays; i++)
-        {
-            Vector3 dir = AngleToDir(currentAngle, true);
-            Debug.DrawLine(transform.position, transform.position + dir * _visionConeRange, Color.green);
-            currentAngle += stepAngle;
-        }
+        List<Vector3> directions = SetupRaycastsDirections(_visionConeAngle, _numberOfRays, true);
+        DrawFieldOfView(directions);
     }
 
     private List<Vector3> SetupRaycastsDirections(float viewAngleInDeg, int numberOfRays, bool isAngleLocal)
@@ -60,7 +51,6 @@ public class PlayerVision : MonoBehaviour
             directions.Add(dir);
             currentAngle += stepAngle;
         }
-
         return directions;
     }
 
@@ -69,6 +59,7 @@ public class PlayerVision : MonoBehaviour
         int vertexCount = raycastsDirections.Count + 1;
         Vector3[] vertices = new Vector3[vertexCount];
         int[] triangles = new int[(vertexCount - 2) * 3];
+        Vector3 oldDir = Vector3.zero;
 
         vertices[0] = Vector3.zero;
 
@@ -81,7 +72,7 @@ public class PlayerVision : MonoBehaviour
             }
             else
             {
-                vertices[i + 1] = raycastsDirections[i] * _visionConeRange;
+                vertices[i + 1] = transform.InverseTransformDirection(raycastsDirections[i]) * _visionConeRange;
             }
 
             if (i < vertexCount - 2)
@@ -89,7 +80,9 @@ public class PlayerVision : MonoBehaviour
                 triangles[i * 3] = 0;
                 triangles[i * 3 + 1] = i + 1;
                 triangles[i * 3 + 2] = i + 2;
-            }            
+            }
+
+            //oldDir = raycastsDirections[i];
         }
 
         _foVMesh.Clear();
@@ -113,5 +106,17 @@ public class PlayerVision : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 PointA;
+        public Vector3 PointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
     }
 }
