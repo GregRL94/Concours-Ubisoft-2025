0579eea1f0565b049f9ddcc87da59e7c8c416b1d
GregRL94 <largreg1994@gmail.com>
2025-03-22 01:00:37 -0400

    Field of view fully implemented
    - Implemented Edge resolve algorithm
    - Implemented Agent detection when they either enter the FoV or the proximity detection radius

--- Assets/Scripts/Players/PlayerVision.cs
+++ Assets/Scripts/Players/PlayerVision.cs
@@ -6,13 +6,41 @@ using UnityEngine;
 
 public class PlayerVision : MonoBehaviour
 {
+    public struct FoVCastInfo
+    {
+        public bool Hit;
+        public Vector3 Point;
+        public float Dist;
+        public float Angle;
+
+        public FoVCastInfo(bool hit, Vector3 point, float dist, float angle)
+        {
+            Hit = hit;
+            Point = point;
+            Dist = dist;
+            Angle = angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 PointA;
+        public Vector3 PointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+    }
+
     [Header("LAYER MASKS")]
     [SerializeField] LayerMask _obstaclesMask;
     [SerializeField] LayerMask _gameAgentsMask;
     [SerializeField] MeshFilter _foVMeshFilter;
     [Space]
     [Header("PROXIMITY DETECTION")]
-    [SerializeField, Range(0f, 5f)] private float _radius;
+    [SerializeField, Range(0f, 5f)] private float _proximityRadius;
     [Space]
     [Header("VISION CONE")]
     [SerializeField, Range(0f, 20f)] private float _visionConeRange;
@@ -20,60 +48,105 @@ public class PlayerVision : MonoBehaviour
     [Space]
     [Header("DETECTION RAYS")]
     [SerializeField] int _numberOfRays;
+    [SerializeField, Range(0.1f, 1f)] float _edgeResolveDistThreshold;
+    [SerializeField, Range(0f, 10f)] int _edgeResolveIterations;
 
+    private GameObject _foVGameObj;
     private Mesh _foVMesh;
+    private SphereCollider _foVCollider;
+
+    private List<GameObject> _elligibleObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         _foVMesh = new Mesh{name = "Field Of View Mesh"};
         _foVMeshFilter.mesh = _foVMesh;
+        _foVGameObj = _foVMeshFilter.gameObject;
+
+        _foVCollider = _foVGameObj.AddComponent<SphereCollider>();
+        _foVCollider.radius = _visionConeRange;
+        _foVCollider.isTrigger = true;
+
+        _elligibleObjects = new List<GameObject>();
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        List<Vector3> directions = SetupRaycastsDirections(_visionConeAngle, _numberOfRays, true);
-        DrawFieldOfView(directions);
+        DrawFieldOfView(_visionConeAngle, _numberOfRays);
+        VisibleObjects();
     }
 
-    private List<Vector3> SetupRaycastsDirections(float viewAngleInDeg, int numberOfRays, bool isAngleLocal)
+    private List<GameObject> VisibleObjects()
     {
-        List<Vector3> directions = new List<Vector3>();
+        List<GameObject> visibleObjects = new List<GameObject>();
+
+        foreach(GameObject obj in _elligibleObjects)
+        {
+            if (Vector3.Distance(transform.position, obj.transform.position) <= _proximityRadius)
+            {
+                visibleObjects.Add(obj);
+                continue;
+            }
+
+            Vector3 dir = obj.transform.position - transform.position;
+            float angle = Vector3.Angle(transform.forward, dir);
+            if (angle <= _visionConeAngle / 2)
+            {
+                if (!Physics.Raycast(transform.position, dir, Vector3.Distance(transform.position, obj.transform.position), _obstaclesMask))
+                {
+                    Debug.DrawLine(transform.position, obj.transform.position, Color.green);
+                    visibleObjects.Add(obj);
+                }
+            }
+        }
+        return new List<GameObject>();
+    }
+
+    private void DrawFieldOfView(float viewAngleInDeg, int numberOfRays)
+    {
+        List<Vector3> hitPoints = new List<Vector3>();
 
         float startAngle = -viewAngleInDeg / 2;
         float stepAngle = viewAngleInDeg / numberOfRays;
         float currentAngle = startAngle;
+        FoVCastInfo oldFoVCast = new FoVCastInfo();
 
         for (int i = 0; i <= numberOfRays; i++)
         {
-            Vector3 dir = AngleToDir(currentAngle, isAngleLocal);
-            directions.Add(dir);
-            currentAngle += stepAngle;
-        }
-        return directions;
-    }
+            FoVCastInfo currentFoVCast = FoVCast(currentAngle);
 
-    private void DrawFieldOfView(List<Vector3> raycastsDirections)
-    {
-        int vertexCount = raycastsDirections.Count + 1;
+            if (i > 0)
+            {
+                bool edgeDistThresholdExceeded = Mathf.Abs(currentFoVCast.Dist - oldFoVCast.Dist) > _edgeResolveDistThreshold;
+
+                if ((oldFoVCast.Hit != currentFoVCast.Hit) || (oldFoVCast.Hit && currentFoVCast.Hit && edgeDistThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldFoVCast, currentFoVCast);
+                    if (edge.PointA != Vector3.zero)
+                    {
+                        hitPoints.Add(edge.PointA);
+                    }
+                    if (edge.PointB != Vector3.zero)
+                    {
+                        hitPoints.Add(edge.PointB);
+                    }
+                }
+            }
+            hitPoints.Add(currentFoVCast.Point);
+            currentAngle += stepAngle;
+            oldFoVCast = currentFoVCast;
+        }
+
+        int vertexCount = hitPoints.Count + 1;
         Vector3[] vertices = new Vector3[vertexCount];
         int[] triangles = new int[(vertexCount - 2) * 3];
-        Vector3 oldDir = Vector3.zero;
 
         vertices[0] = Vector3.zero;
 
         for (int i = 0; i < vertexCount-1; i++)
         {
-            Ray ray = new Ray(transform.position, raycastsDirections[i]);
-            if (Physics.Raycast(ray, out RaycastHit hit, _visionConeRange, _obstaclesMask))
-            {
-                vertices[i + 1] = transform.InverseTransformPoint(hit.point);
-            }
-            else
-            {
-                vertices[i + 1] = transform.InverseTransformDirection(raycastsDirections[i]) * _visionConeRange;
-            }
+            vertices[i + 1] = transform.InverseTransformPoint(hitPoints[i]);
 
             if (i < vertexCount - 2)
             {
@@ -81,8 +154,6 @@ public class PlayerVision : MonoBehaviour
                 triangles[i * 3 + 1] = i + 1;
                 triangles[i * 3 + 2] = i + 2;
             }
-
-            //oldDir = raycastsDirections[i];
         }
 
         _foVMesh.Clear();
@@ -98,25 +169,74 @@ public class PlayerVision : MonoBehaviour
         float dirX = Mathf.Sin(angleInDeg);
         float dirZ = Mathf.Cos(angleInDeg);
 
-        if (isAngleLocal) { return transform.TransformDirection(new Vector3(dirX, 0f, dirZ)); }
+        if (isAngleLocal)
+        {
+            return transform.TransformDirection(new Vector3(dirX, 0f, dirZ));
+        }
+
         return new Vector3(dirX, 0f, dirZ);
+    }
+
+    private FoVCastInfo FoVCast(float localAngle)
+    {
+        Vector3 dir = AngleToDir(localAngle, true);
+        Ray ray = new Ray(transform.position, dir);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _visionConeRange, _obstaclesMask))
+        {
+            return new FoVCastInfo(true, hit.point, hit.distance, localAngle);
+        }
+        return new FoVCastInfo(false, transform.position + dir * _visionConeRange, _visionConeRange, localAngle);
+    }
+
+    private EdgeInfo FindEdge(FoVCastInfo minFoVCast, FoVCastInfo maxFoVCast)
+    {
+        float minAngle = minFoVCast.Angle;
+        float maxAngle = maxFoVCast.Angle;
+
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < _edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            FoVCastInfo resolveCast = FoVCast(angle);
+            bool edgeDistThresholdExceeded = Mathf.Abs(minFoVCast.Dist - resolveCast.Dist) > _edgeResolveDistThreshold;
+
+            if (resolveCast.Hit == minFoVCast.Hit && !edgeDistThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = resolveCast.Point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = resolveCast.Point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_elligibleObjects.Contains(other.gameObject))
+        {
+            _elligibleObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_elligibleObjects.Contains(other.gameObject))
+        {
+            _elligibleObjects.Remove(other.gameObject);
+        }
     }
 
     public void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, _radius);
-    }
-
-    public struct EdgeInfo
-    {
-        public Vector3 PointA;
-        public Vector3 PointB;
-
-        public EdgeInfo(Vector3 pointA, Vector3 pointB)
-        {
-            PointA = pointA;
-            PointB = pointB;
-        }
+        Gizmos.DrawWireSphere(transform.position, _proximityRadius);      
     }
 }
