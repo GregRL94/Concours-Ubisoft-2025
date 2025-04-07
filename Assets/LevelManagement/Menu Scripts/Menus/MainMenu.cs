f111a69e171decccff95a17b62b2bd597eeb3bfb
AkuroP <thomas.kang2002@gmail.com>
2025-04-07 14:04:51 -0400

    UI SFX Integrated

--- Assets/LevelManagement/Menu Scripts/Menus/MainMenu.cs
+++ Assets/LevelManagement/Menu Scripts/Menus/MainMenu.cs
@@ -15,6 +15,7 @@ namespace LevelManagement
 
         public void OnPlayPressed()
         {
+            AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["UI Confirm"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
             StartCoroutine(OnPlayPressedRoutine());
         }
 
@@ -22,23 +23,26 @@ namespace LevelManagement
         {
             if(startTransitionPrefab)
                 TransitionFader.PlayTransition(startTransitionPrefab);
+            
             yield return new WaitForSeconds(_playDelay);
             LevelLoader.LoadNextLevel();
         }
 
         public void OnSettingsPressed()
         {
-
+            AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["UI Open Menu"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
             SettingsMenu.Open();
         }
 
         public void OnCreditsPressed()
         {
+            AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["UI Open Menu"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
             CreditsScreen.Open();
         }
 
         public override void OnBackPressed()
         {
+            AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["UI Close Menu"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
             Application.Quit();
             #if UNITY_EDITOR
                         UnityEditor.EditorApplication.isPlaying = false; // Exit option for editor 
